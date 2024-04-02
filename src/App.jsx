95c9aeb20b58a78c3c2c51dcfe10f74c8358b004
hariharan <harishmohan138@gmail.com>
2024-04-02 13:32:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="card-container">
+      <div className="image-container"></div>
+      <div className="gradient-container"></div>
+      <div className="card-content-container">@..HIMALAYAS..@</div>
+    </div>
+  );
+}
+
+export default App;
